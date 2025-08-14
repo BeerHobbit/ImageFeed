c897641cb0c13c44156a33edfbe45ff64265049c
BeerHobbit <kovalenko537@gmail.com>
2025-08-14 20:59:37 +0300

    Gradient layer and shadow for like button were added

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -8,5 +8,6 @@ final class ImagesListCell: UITableViewCell {
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var gradientView: UIView!
+    
 }
 
